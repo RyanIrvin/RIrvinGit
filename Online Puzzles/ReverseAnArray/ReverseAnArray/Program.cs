14563bc5900de0f108f7342cb563be7ae7279913
RyanIrvin <40035854+RyanIrvin@users.noreply.github.com>
2019-06-21 17:32:56 -0400

    ReverseAnArray
    Method to reverse an array in place

---
+++ Online Puzzles/ReverseAnArray/ReverseAnArray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            reverse(arr);
+        }
+
+        static int[] reverse(int[] arr)
+        {
+            int tempNum = 0;
+
+            for (int i = 0, j = arr.Length - 1; i < arr.Length; i++, j--)
+            {
+                if (!(i == j))
+                {
+                    tempNum = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tempNum;
+                }
+                else
+                    break;
+            }
+
+            return arr;
+        }
+    }
+}
