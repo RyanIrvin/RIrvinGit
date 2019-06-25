409345cf5c4b99cd11243071064b284dbe1e2a11
RyanIrvin <40035854+RyanIrvin@users.noreply.github.com>
2019-06-25 15:02:56 -0400

    HowManyVowels
    Counts number of vowels in a string

---
+++ Online Puzzles/HowManyVowels/HowManyVowels/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowManyVowels
+{
+    /*Create a function that takes a string and returns the number (count) of vowels contained within it.
+
+        Examples
+        CountVowels("Celebration") ➞ 5
+        
+        CountVowels("Palm") ➞ 1
+        
+        CountVowels("Prediction") ➞ 4
+        Notes
+        a, e, i, o, u are considered vowles (not y).
+        All test cases are one word and only contain letters.
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "My test string at eMoney Advisor";
+
+            Console.WriteLine(CountVowels(str));
+            Console.ReadKey();
+        }
+
+        public static int CountVowels(string str)
+        {
+            string vowels = "aeiou";
+            int vowelCount = 0;
+
+            return str.Count(letter => vowels.Contains(letter));
+            //Rewritten as a LINQ Lambda expression
+
+            /*
+            foreach (char letter in str)
+                if (vowels.Contains(letter.ToString()))
+                    vowelCount++;
+
+            return vowelCount;
+            */
+        }
+    }
+}
