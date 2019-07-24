b71f9e8f6a0ed81101cdd6cac0a20b25dc462bac
RyanIrvin <40035854+RyanIrvin@users.noreply.github.com>
2019-07-24 01:55:28 -0400

    Added NewHirePDFExtractor
    Tool to extract text information for new hires from PX (HR) Onboarding forms

---
+++ NewHirePDFExtractor/NewHirePDFExtractor/Program.cs
@@ -0,0 +1,36 @@
+﻿using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NewHirePDFExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo folderLocation = new DirectoryInfo(@"C:\Users\Ryan\Desktop\NewHirePDFs");
+            List<FileInfo> Files = new List<FileInfo>(folderLocation.GetFiles("*.pdf"));
+            Regex regexAlphanumeric = new Regex("[^ a-zA-z0-9]");
+
+            foreach (FileInfo file in Files)
+            {
+                PdfReader reader = new PdfReader($"{folderLocation}\\{file}");
+
+                string fullName = regexAlphanumeric.Replace(reader.AcroFields.GetField("New Hire Full Name"), "").Trim();
+                string managerName = regexAlphanumeric.Replace(reader.AcroFields.GetField("Manager Name"), "").Trim();
+                string title = regexAlphanumeric.Replace(reader.AcroFields.GetField("Title"), "").Trim();
+                string seatLocation = String.IsNullOrEmpty(reader.AcroFields.GetField("Location")) ? reader.AcroFields.GetField("Location").Trim() : "NEED LOCATION";
+
+
+                Console.WriteLine($"{fullName}, {managerName}, {title}, {seatLocation}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
