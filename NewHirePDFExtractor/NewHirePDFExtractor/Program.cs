8baff48ec6c8f1ce97d00fbc351e31b78e136d09
RyanIrvin <40035854+RyanIrvin@users.noreply.github.com>
2019-07-25 19:57:07 -0400

    NewHirePDFExtractor Update
    Cleaned up debug code, added comments

--- NewHirePDFExtractor/NewHirePDFExtractor/Program.cs
+++ NewHirePDFExtractor/NewHirePDFExtractor/Program.cs
@@ -14,7 +14,7 @@ namespace NewHirePDFExtractor
         static void Main(string[] args)
         {
             StringBuilder tempString = new StringBuilder();
-            DirectoryInfo folderLocation = new DirectoryInfo(@"C:\Users\rirvin\Downloads\7-29\");
+            DirectoryInfo folderLocation = new DirectoryInfo(@"C:\Users\Ryan\Downloads\7-29\");
             List<FileInfo> Files = new List<FileInfo>(folderLocation.GetFiles("*.pdf"));
             Regex regexAlphanumeric = new Regex("[^ a-zA-z0-9]");
             string csvFile = "Test.csv";
@@ -23,38 +23,25 @@ namespace NewHirePDFExtractor
             {
                 PdfReader reader = new PdfReader($"{folderLocation}{file}");
 
+                //Pulls PDF field values from their field names
                 string fullName = regexAlphanumeric.Replace(reader.AcroFields.GetField("New Hire Full Name"), "").Trim();
                 string managerName = regexAlphanumeric.Replace(reader.AcroFields.GetField("Manager Name"), "").Trim();
                 string title = regexAlphanumeric.Replace(reader.AcroFields.GetField("Title"), "").Trim();
                 string seatLocation = reader.AcroFields.GetField("Location");
-                bool missingCubeNumber = String.IsNullOrEmpty(reader.AcroFields.GetField("Floor  Cube"));// ? "NEED CUBE NUMBER" : $"***{reader.AcroFields.GetField("Floor  Cube")}***";
 
+                bool missingCubeNumber = String.IsNullOrEmpty(reader.AcroFields.GetField("Floor  Cube"));
                 bool isContractor = reader.AcroFields.GetField("Contractor Agency").Equals("On");
 
-                Console.Write($"\n{fullName}, {managerName}, {title}, {seatLocation}, ");
+                tempString.Append($"{fullName}, {title}, {managerName}, {seatLocation}, ");
 
-                tempString.Append($"{fullName}, {managerName}, {title}, {seatLocation}, ");
-
-                if(missingCubeNumber)
-                {   
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write("NEED CUBE NUMBER");
-                    Console.ResetColor();
-
+                if(missingCubeNumber) //Check if seating location has been provided on Onboard Form
                     tempString.Append("NEED CUBE NUMBER");
-                }
                 else
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write($"{reader.AcroFields.GetField("Floor  Cube")}");
-                    Console.ResetColor();
-
                     tempString.Append($"{reader.AcroFields.GetField("Floor  Cube")}");
-                }
 
-                if(isContractor)
+                if(isContractor) //Check if new hire is a contractor or EMA employee
                 {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Yellow;  //debuging
                     Console.Write($", *CONTRACTOR: {reader.AcroFields.GetField("2")}*");
                     Console.ResetColor();
 
