1c4c73efdfa76bdf7978096fcc070ab7de926cf4
Ryan Irvin <rirvin5@gmail.com>
2020-09-20 14:50:25 -0400

    Add ItemLookupDatabase

---
+++ ItemLookUpGUI/ItemLookUpGUI/ItemLookup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemLookUpGUI
+{
+    class ItemLookup
+    {
+        private string Input;
+
+        
+        public ItemLookup(string input)
+        {
+            Input = input;
+        }
+    }
+}
--- ItemLookUpGUI/ItemLookUpGUI/UserControl1.Designer.cs
+++ ItemLookUpGUI/ItemLookUpGUI/UserControl1.Designer.cs
@@ -28,11 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(17, 19);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.InputTextBox.TabIndex = 0;
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(123, 19);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(75, 23);
+            this.SubmitButton.TabIndex = 1;
+            this.SubmitButton.Text = "Enter";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(3, 61);
+            this.OutputTextBox.Multiline = true;
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.ReadOnly = true;
+            this.OutputTextBox.Size = new System.Drawing.Size(195, 114);
+            this.OutputTextBox.TabIndex = 2;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.InputTextBox);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(200, 185);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.TextBox OutputTextBox;
     }
 }
