003392c48f10af8de3f5825b4606fb53bd87d4e1
Ryan Irvin <rirvin5@gmail.com>
2020-09-22 11:32:37 -0400

    Add getting ItemList by itemID

--- ItemLookUpGUI/ItemLookUpGUI/ItemLookup.cs
+++ ItemLookUpGUI/ItemLookUpGUI/ItemLookup.cs
@@ -16,10 +16,18 @@ namespace ItemLookupGui
             db = new ItemLookupEntities();
         }
 
-        public List<string> FindItems(string itemName)
+
+        public List<string> FindItems(string itemIdentifier)
         {
-            List<ItemList> itemListList = db.ItemLists.Where(itemList => itemList.ItemName.Contains(itemName)).ToList();
-            return itemListList.SelectMany(itemList => new List<string> { $"{itemList.ItemID} - {itemList.ItemName}" }).ToList();
+            List<ItemList> itemListList = GetItemList(itemIdentifier.Trim());
+            return itemListList.Count > 0 ? itemListList.SelectMany(itemList => new List<string> { $"{itemList.ItemID} - {itemList.ItemName}" }).ToList() : new List<string> { "No Results Found." };
+        }
+
+        private List<ItemList> GetItemList(string itemIdentifier)
+        {
+            var itemIDList = db.ItemLists.Where(itemList => itemList.ItemID.ToString().Contains(itemIdentifier)).ToList();
+            var itemNameList = db.ItemLists.Where(itemList => itemList.ItemName.Contains(itemIdentifier)).ToList();
+            return itemIDList.Count > 0 ? itemIDList : itemNameList;
         }
     }
 }
